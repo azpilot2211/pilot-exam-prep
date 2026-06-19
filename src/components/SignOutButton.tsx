70b6949336abd540f8892c824152cdc8763da8b6
Michael Driggs <azpilot@example.com>
2026-06-18 22:19:39 -0600

    added cursor pointer to sign out button

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -19,7 +19,7 @@ export function SignOutButton({ onSignOut }: { onSignOut?: () => void }) {
   return (
     <button
       onClick={handleSignOut}
-      className="text-md font-semibold text-slate-500 underline hover:text-slate-700 transition-colors"
+      className="text-md font-semibold text-slate-500 underline hover:text-slate-700 hover:cursor-pointer transition-colors"
     >
       Sign out
     </button>
